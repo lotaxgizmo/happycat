1d3f465e184db20997b829c15bec9b6ca92c3fa8
Lotachi <lotaoko2@gmail.com>
2024-03-15 15:40:53 +0100

    stuff

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Hero from "./components/Hero";
 import Welcome from "./components/Welcome";
 import Tokenomics from "./components/Tokenomics";
 import Howto from "./components/Howto";
+import Header from "./components/Header";
 
 
 
 function App() {
   return (
     <div className="overflow-clip">
+      <Header />
       <Hero />
       <Welcome />
       <Tokenomics />
