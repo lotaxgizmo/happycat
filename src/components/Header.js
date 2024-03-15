a7de50bd1d7d374b5c4b76e46b9c2d4e51c0a4e1
Lotachi <lotaoko2@gmail.com>
2024-03-15 16:07:43 +0100

    stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import happyblackbuy from '../assets/happyblackbuy.svg'
+import welcomecat from '../assets/welcomecat.svg'
+import Buy from '../assets/Buy.svg'
+import Tokenomics from '../assets/Tokenomics.svg'
+import About from '../assets/About.svg'
+import Home from '../assets/Home.svg'
 
 function Header() {
     return (
-        <div>
-            <div className="header ">
-                hello
+        <div className=' items-center justify-center pt-3 hidden lg:flex'>
+            <div className="header bg-[#56D6E5] w-11/12 h-16 rounded-full flex flex-row items-center justify-between px-10">
+                <img src={welcomecat} className='w-14' alt="" />
+                <div className="header border-2 border-black w-7/12 h-14 rounded-full flex flex-row items-center justify-between px-10">
+
+                </div>
+                <img src={happyblackbuy} className='' alt="" />
+
             </div>
         </div>
     )
